20b1bd18fecbb20594823893d057a112c139c651
wxn0brP <toja.filiper@gmail.com>
2026-03-12 22:02:21 +0100

    fix: gen id

--- utils/utils.php
+++ utils/utils.php
@@ -35,11 +35,16 @@ function escapeIdentifier(string $name, string $type): string
 }
 
 /**
- * Generate a unique ID (similar to MongoDB ObjectId style)
+ * Generate a ValtheraDB ID
  */
 function genId(): string
 {
-    return bin2hex(random_bytes(12));
+    $time = base_convert((string) (microtime(true) * 1000), 10, 36);
+
+    $part1 = base_convert((string) rand(0, 35), 10, 36);
+    $part2 = base_convert((string) rand(0, 35), 10, 36);
+
+    return "{$time}-{$part1}-{$part2}";
 }
 
 /**
